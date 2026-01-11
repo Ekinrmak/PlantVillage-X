90dd45a0bbdc937fd3323da1fbd03eece6201694
Ekin Irmak <73664795+Ekinrmak@users.noreply.github.com>
2026-01-11 15:38:21 +0300

    Create gradcam.py

---
+++ src/gradcam.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+import numpy as np
+import cv2
+
+def make_gradcam_heatmap(img_array, model, last_conv_layer_name):
+    # (Buraya Notebook'taki Grad-CAM fonksiyonunun aynısını yapıştır)
+    # Kodun tamamını tekrar yazmama gerek yok, mantığı anladın.
+    pass
